74f84ad5fc80af025ef70f0235d0214b1305df71
Noah-1711 <noahnirmal11@gmail.com>
2022-01-02 17:01:13 +0530

    28-12-21

--- EvenElementDecreasing.java
+++ EvenElementDecreasing.java
@@ -15,14 +15,13 @@ public class EvenElementDecreasing {
 		}
 
 		public static void main(String args[]) {
+			
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter the number");
+		System.out.println("Enter the number:");
 		int n = scan.nextInt();
 		System.out.println("The Even elements are");
 
 		evendecresing(n);
-
-
 		}
 
 }
