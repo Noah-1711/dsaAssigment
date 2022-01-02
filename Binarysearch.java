a71fa3d64fb84e12716f6b6504522c1678f57c07
Noah-1711 <noahnirmal11@gmail.com>
2022-01-02 17:24:49 +0530

    20-12-21

--- Binarysearch.java
+++ Binarysearch.java
@@ -3,46 +3,34 @@ package dsaassignment;
 public class Binarysearch {
 
 	public static void main(String[] args) {
-		
 
-			  int[] arr = {2,3,5,6,9,11,13,15};
-			  
-			  int start = 0;
-			  
-			  int target = 11;
-			  
-			  int end = arr.length-1;
-			  
-			  int ceiling = ceiling(arr, start, end, target);
-			  
-			  
-			  System.out.println(" ceiling= "+ceiling );
+		int[]num= {1,2,3,5,4,17};
+		int start=0;
+		int target=17;
+		int end=num.length-1;
+		 
+		 while(start<=end) {
+			 int mid=start+(end-start)/2;
 			 
-
-		}
-
-		
-
-		private static int ceiling(int[] arr, int start, int end, int target) {
-
-			while (start <= end) {
-
-				int mid = start + (end - start) / 2;
-
-				if (arr[mid] < target) {
-
-					start = mid + 1;
-
-				} else {
-					end = mid - 1;
-				}
-			}
-
-			return arr[start];
-		}
-
-
-
+			 
+			 if(target==num[mid]){
+				 System.out.println("index is "+mid);
+				 break;
+			 }
+			 else if(target<num[mid]){
+				 end=mid-1;
+				 if(target!=num[mid]) {
+					 System.out.println(" target not found");
+				 }
+			 }
+			 else {
+				 start=mid+1;
+			 } 
+			 
+		 }
+	
+		 
+	}
 
 	}
 
