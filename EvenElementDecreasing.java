b4aa925a15e5a5e7ea618637b8c1bbcfc7e999bd
Noah-1711 <noahnirmal11@gmail.com>
2022-01-02 16:46:38 +0530

    28-12-21

---
+++ EvenElementDecreasing.java
@@ -0,0 +1,28 @@
+package dsaassignment;
+import java.util.Scanner;
+
+public class EvenElementDecreasing {
+	static public void evendecresing(int a) {
+
+		if(a<=0) {
+		return ;
+		}
+		if(a%2==0) {
+		System.out.println(a);
+		}
+		evendecresing(a-1);
+
+		}
+
+		public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n = scan.nextInt();
+		System.out.println("The Even elements are");
+
+		evendecresing(n);
+
+
+		}
+
+}
