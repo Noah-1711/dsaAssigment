25ae5dc29e5bfea0925ac4d97048daff24442ed6
Noah-1711 <noahnirmal11@gmail.com>
2022-01-09 14:33:49 +0530

    6-01-21

---
+++ RemoveAdjacent.java
@@ -0,0 +1,52 @@
+package dsaassignment;
+import java.util.Stack;
+
+public class RemoveAdjacent {
+	public void removeAdjacentDuplicate(String text)
+	{
+		if (text.length() == 0)
+		{
+			return;
+		}
+		// Define use useful resultant variables
+		String result = "";
+		int element = 0;
+		// Use to collect string characters
+		Stack < Character > record = new Stack < Character > ();
+		// Find all unique adjacent characters
+		while (element < text.length())
+		{
+			if (record.isEmpty() || record.peek() != text.charAt(element))
+			{
+				// When stack is empty or
+				// Two adjacent characters are not same
+				record.push(text.charAt(element));
+			}
+			else
+			{
+				// Two adjacent characters are same
+				// Remove stack element
+				record.pop();
+			}
+			// visit to next character
+			element++;
+		}
+		// Collect non adjacent duplicate characters
+		while (!record.isEmpty())
+		{
+			result = record.peek() + result;
+			record.pop();
+		}
+		// Display given text
+		System.out.println(" Given Text : " + text);
+		// Display calculated result
+		System.out.println(" Remove Adjacent Duplicate : [" + result + "]");
+	}
+	public static void main(String[] args)
+	{
+		RemoveAdjacent  task = new RemoveAdjacent ();
+		
+		task.removeAdjacentDuplicate("Geekster");
+	
+	}
+}
---
+++ ReverseStringUsingStack.java
@@ -0,0 +1,29 @@
+package dsaassignment;
+import java.util.Stack;
+
+public class ReverseStringUsingStack {
+    public static String reverse(String str)
+    {
+        Stack<Character> stack = new Stack<>();
+ 
+        char[] chars = str.toCharArray();
+        for (char c: chars) {
+            stack.push(c);
+        }
+ 
+       
+        for (int i = 0; i < str.length(); i++) {
+            chars[i] = stack.pop();
+        }
+ 
+        return new String(chars);
+    }
+ 
+    public static void main (String[] args)
+    {
+        String str = "Reverse me";
+ 
+        str = reverse(str);
+        System.out.println(str);
+    }
+}
