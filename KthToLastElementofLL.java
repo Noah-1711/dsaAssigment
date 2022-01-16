8abd820b463732d44ea2d170be41fe693524d6b0
Noah-1711 <noahnirmal11@gmail.com>
2022-01-16 19:12:26 +0530

    11-1-22

--- KthToLastElementofLL.java
+++ KthToLastElementofLL.java
@@ -1,6 +1,18 @@
 package dsaassignment;
 
+
+  
+
 public class KthToLastElementofLL {
+	class Node{
+		 int data;
+		 Node next;
+		 public Node(int data){
+		 this.data = data;
+		 next = null;
+		 }
+	 }
+	
 	public int kthByRecursion(Node head, int k){
 		 if(head==null){
 		 return 0;
@@ -34,29 +46,26 @@ public class KthToLastElementofLL {
 		 System.out.print(" " + n.data);
 		 n=n.next;
 		 }
-		 }
-		 public static void main(String args[]){
-		 Node n = new Node(1);
-		 n.next = new Node(2);
-		 n.next.next = new Node(8);
-		 n.next.next.next = new Node(3);
-		 n.next.next.next.next = new Node(7);
-		 n.next.next.next.next.next = new Node(0);
-		 n.next.next.next.next.next.next = new Node(4);
-		 System.out.print("Original List : ");
-		 KthToLastElementofLL rm = new KthToLastElementofLL();
-		 rm.display(n); 
-		 System.out.print("\n Recursion::3rd Element from the end is : ");
-		 rm.kthByRecursion(n, 3);
-		 System.out.print("\n Iteration::5th Element from the end is : " + rm.kthByIteration(n, 5));
 		 
 		 }
-		}
-		class Node{
-		 int data;
-		 Node next;
-		 public Node(int data){
-		 this.data = data;
-		 next = null;
-		 }
-}
+		 public static void main(String args[]){
+			 Node n = new Node(1);
+			 n.next = new Node(2);
+			 n.next.next = new Node(8);
+			 n.next.next.next = new Node(3);
+			 n.next.next.next.next = new Node(7);
+			 n.next.next.next.next.next = new Node(0);
+			 n.next.next.next.next.next.next = new Node(4);
+			 System.out.print("Original List : ");
+			 KthToLastElementofLL rm = new KthToLastElementofLL();
+			 rm.display(n); 
+			 System.out.print("\n Recursion::3rd Element from the end is : ");
+			 rm.kthByRecursion(n, 3);
+			 System.out.print("\n Iteration::5th Element from the end is : " + rm.kthByIteration(n, 5));
+			 
+			 }
+			}
+ 
+		 
+		
+
